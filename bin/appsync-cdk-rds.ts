c8688106477fe0479af06368f8811316327cb958
Ryan <dsouza.ryan.11@gmail.com>
2020-11-19 19:20:03 +0530

    initial commit after fork

--- bin/appsync-cdk-rds.ts
+++ bin/appsync-cdk-rds.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
-import { AppsyncCdkRdsStack } from '../lib/appsync-cdk-rds-stack';
+import 'source-map-support/register'
+import * as cdk from '@aws-cdk/core'
+import { AppsyncCdkRdsStack } from '../lib/appsync-cdk-rds-stack'
 
-const app = new cdk.App();
-new AppsyncCdkRdsStack(app, 'AppsyncCdkRdsStack');
+const app = new cdk.App()
+new AppsyncCdkRdsStack(app, 'AppsyncCdkRdsStack')
--- lib/appsync-cdk-rds-stack.ts
+++ lib/appsync-cdk-rds-stack.ts
@@ -1,12 +1,12 @@
-import * as cdk from '@aws-cdk/core';
-import * as ec2 from '@aws-cdk/aws-ec2';
-import * as lambda from '@aws-cdk/aws-lambda';
-import * as rds from '@aws-cdk/aws-rds';
-import * as appsync from '@aws-cdk/aws-appsync';
+import * as cdk from '@aws-cdk/core'
+import * as ec2 from '@aws-cdk/aws-ec2'
+import * as lambda from '@aws-cdk/aws-lambda'
+import * as rds from '@aws-cdk/aws-rds'
+import * as appsync from '@aws-cdk/aws-appsync'
 
 export class AppsyncCdkRdsStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    super(scope, id, props)
 
     // Create the AppSync API
     const api = new appsync.GraphqlApi(this, 'Api', {
@@ -16,22 +16,30 @@ export class AppsyncCdkRdsStack extends cdk.Stack {
         defaultAuthorization: {
           authorizationType: appsync.AuthorizationType.API_KEY,
           apiKeyConfig: {
-            expires: cdk.Expiration.after(cdk.Duration.days(365))
-          }
+            expires: cdk.Expiration.after(cdk.Duration.days(365)),
+          },
         },
       },
-    });
+    })
 
     // Create the VPC needed for the Aurora Serverless DB cluster
-    const vpc = new ec2.Vpc(this, 'BlogAppVPC');
+    const vpc = new ec2.Vpc(this, 'BlogAppVPC')
     // Create the Serverless Aurora DB cluster; set the engine to Postgres
     const cluster = new rds.ServerlessCluster(this, 'AuroraBlogCluster', {
       engine: rds.DatabaseClusterEngine.AURORA_POSTGRESQL,
-      parameterGroup: rds.ParameterGroup.fromParameterGroupName(this, 'ParameterGroup', 'default.aurora-postgresql10'),
+      parameterGroup: rds.ParameterGroup.fromParameterGroupName(
+        this,
+        'ParameterGroup',
+        'default.aurora-postgresql10'
+      ),
       defaultDatabaseName: 'BlogDB',
       vpc,
-      scaling: { autoPause: cdk.Duration.seconds(0) } // Optional. If not set, then instance will pause after 5 minutes 
-    });
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      scaling: {
+        // Optional. If not set, then instance will pause after 5 minutes
+        autoPause: cdk.Duration.seconds(0),
+      },
+    })
 
     // Create the Lambda function that will map GraphQL operations into Postgres
     const postFn = new lambda.Function(this, 'MyFunction', {
@@ -43,45 +51,45 @@ export class AppsyncCdkRdsStack extends cdk.Stack {
         CLUSTER_ARN: cluster.clusterArn,
         SECRET_ARN: cluster.secret?.secretArn || '',
         DB_NAME: 'BlogDB',
-        AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1'
+        AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
       },
-    });
+    })
     // Grant access to the cluster from the Lambda function
-    cluster.grantDataApiAccess(postFn);
+    cluster.grantDataApiAccess(postFn)
     // Set the new Lambda function as a data source for the AppSync API
-    const lambdaDs = api.addLambdaDataSource('lambdaDatasource', postFn);
+    const lambdaDs = api.addLambdaDataSource('lambdaDatasource', postFn)
 
     // Map the resolvers to the Lambda function
     lambdaDs.createResolver({
       typeName: 'Query',
-      fieldName: 'listPosts'
-    });
+      fieldName: 'listPosts',
+    })
     lambdaDs.createResolver({
       typeName: 'Query',
-      fieldName: 'getPostById'
-    });
+      fieldName: 'getPostById',
+    })
     lambdaDs.createResolver({
       typeName: 'Mutation',
-      fieldName: 'createPost'
-    });
+      fieldName: 'createPost',
+    })
     lambdaDs.createResolver({
       typeName: 'Mutation',
-      fieldName: 'updatePost'
-    });
+      fieldName: 'updatePost',
+    })
     lambdaDs.createResolver({
       typeName: 'Mutation',
-      fieldName: 'deletePost'
-    });
+      fieldName: 'deletePost',
+    })
 
     // CFN Outputs
     new cdk.CfnOutput(this, 'AppSyncAPIURL', {
-      value: api.graphqlUrl
-    });
+      value: api.graphqlUrl,
+    })
     new cdk.CfnOutput(this, 'AppSyncAPIKey', {
-      value: api.apiKey || ''
-    });
+      value: api.apiKey || '',
+    })
     new cdk.CfnOutput(this, 'ProjectRegion', {
-      value: this.region
-    });
+      value: this.region,
+    })
   }
 }
