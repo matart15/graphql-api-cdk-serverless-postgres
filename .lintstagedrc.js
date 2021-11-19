532b6876b6785e968474af950cfa1d20d72193e3
matar <matart15@gmail.com>
2021-11-19 12:00:57 +0900

    eslint

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,9 +1,7 @@
 module.exports = {
-  'src/**/*.js': ['eslint --fix', 'git add'],
-  'src/**/*.jsx': ['eslint --fix', 'git add'],
-  'src/**/*.ts': ['eslint --fix', 'git add'],
-  'src/**/*.tsx': ['eslint --fix', 'git add'],
-  'amplify/backend/function/**/*.ts': [
-    'eslint -c .eslintrc.function.js --no-eslintrc --quiet --fix',
+  'lib/**/*.ts': ['eslint --fix', 'git add'],
+  'bin/**/*.tsx': ['eslint --fix', 'git add'],
+  'src/**/*.ts': [
+    'eslint -c .eslintrc.functions.js --quiet --fix',
   ],
 };
--- src/lambdas/listUsers/index.ts
+++ src/lambdas/listUsers/index.ts
@@ -1,7 +1,7 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { createPrismaClient } from '@shared_libs/createPrismaClient';
 // import { v4 as uuidv4 } from 'uuid';
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const handler = async (): Promise<any[]> => {
   const prisma = createPrismaClient();
   console.log('from index');
